692a7504c7734606f8fc251b4eca2514e1d89334
kevin <da_kurlzzzzz@mail.ru>
2022-03-15 16:04:07 +0300

    retyped task 2

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -1,5 +1,53 @@
 #include <iostream>
+#include <string>
+
+class TNode {
+private:
+    static TNode* head;
+    TNode* next;
+    TNode* prev;
+    std::string info;
+
+public:
+    void SetData(std::string str);
+    void AddToList();
+    void PrintAll();
+};
+
+TNode* TNode::head = nullptr;
+
+void TNode::AddToList() {
+    this->next = TNode::head;
+    this->prev = nullptr;
+    if (TNode::head != nullptr) {
+        TNode::head->prev = this;
+    }
+    TNode::head = this;
+}
+
+void TNode::SetData(std::string str) {
+    this->info = str;
+}
+
+void TNode::PrintAll() {
+    for (TNode* cur = TNode::head; cur != this; cur = cur->next) {
+        std::cout << cur->info << std::endl;
+    }
+    std::cout << this->info << std::endl;
+}
 
 int main() {
+    TNode a, b, c, d;
+    a.SetData("Ann");
+    b.SetData("Nick");
+    c.SetData("Lena");
+    d.SetData("Tom");
+
+    a.AddToList();
+    b.AddToList();
+    c.AddToList();
+    d.AddToList();
+
+    b.PrintAll();
     return 0;
 }
